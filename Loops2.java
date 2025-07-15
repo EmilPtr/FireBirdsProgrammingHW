a800d3943da92155740c12fc44a33a4e55311288
LimeTheDerg <emilsahel@gmail.com>
2025-07-15 00:23:28 -0400

    some comments

--- Loops2.java
+++ Loops2.java
@@ -5,10 +5,10 @@ public class Loops2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Create a Scanner object to read input from the user
         String input = sc.nextLine().trim(); // Read user input and trim any leading or trailing whitespace
-        int endP = input.length()-1;
+        int endP = input.length()-1; // Create hypothetical end pointer that will be moved backwards
         while (endP > 0) {
-            System.out.println(input.substring(0, endP));
-            endP--;
+            System.out.println(input.substring(0, endP)); // Prints a portion of the input, from the beginning to the end pointer
+            endP--; // Moves the end pointer backwards by one character
         }
         System.out.println("");
     }       
