76bfbc4ebab5097acaf97f53bdfabc32d4d16b27
LimeTheDerg <emilsahel@gmail.com>
2025-07-15 00:30:00 -0400

    forgor smth

--- IdealTypes.java
+++ IdealTypes.java
@@ -4,4 +4,5 @@ public class IdealTypes {
     private char aSingleLetter = 'a' ; // Ideal datatype for a single letter is char, because it is a single character
     private String robotName = "Rotisserie Chicken"; // Ideal datatype for a name is String, because it is a sequence of characters
     private double volumeOfSphere = (4/3) * Math.PI * Math.pow(5, 3); // Ideal datatype for volume of sphere is double, because it is a large number with decimals, and the formula involves PI and division, which means there will be many decimals
+    private long numberOfNeurons = 1000000000L; // Ideal datatype for number of neurons is long, because it would be a very large number(for most people), and long can hold larger values than int
 }
