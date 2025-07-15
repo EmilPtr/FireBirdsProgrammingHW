d85c78a14e408b076f9e0b38cc81ed8dc811c412
LimeTheDerg <emilsahel@gmail.com>
2025-07-15 00:28:56 -0400

    finished the ideal types thing

---
+++ IdealTypes.java
@@ -0,0 +1,7 @@
+public class IdealTypes {
+    private float money = (float) 1.30; // Ideal datatype for money is float, because it is a large number, with some decimals.
+    private int sumOfSeriesOfNaturalNumbers = 1 + 2 + 3 + 4 + 5; // Ideal datatype for sum of series of natural numbers is int, because it is a medium sized number, and natural numbers do not have decimals
+    private char aSingleLetter = 'a' ; // Ideal datatype for a single letter is char, because it is a single character
+    private String robotName = "Rotisserie Chicken"; // Ideal datatype for a name is String, because it is a sequence of characters
+    private double volumeOfSphere = (4/3) * Math.PI * Math.pow(5, 3); // Ideal datatype for volume of sphere is double, because it is a large number with decimals, and the formula involves PI and division, which means there will be many decimals
+}
